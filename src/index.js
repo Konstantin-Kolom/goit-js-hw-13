16e3d4a693fdf14af163b188263be314c20b934f
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-07-15 23:49:32 +0300

    Start

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import '../node_modules/normalize.css/normalize.css'
+import './sass/main.scss'
+
+import './js/them.js'
+
+// all modules
+import Notiflix from "notiflix";
+console.log(Notiflix);
+
+// one by one
+// import { Notify, Report, Confirm, Loading, Block } from "notiflix";
+
+
