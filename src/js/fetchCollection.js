f9220303f4e46b63475227757699e0bc169f7341
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2021-07-18 19:15:46 +0300

    готово

--- src/js/fetchCollection.js
+++ src/js/fetchCollection.js
@@ -1,16 +1,17 @@
 import { API_KEY } from './key.js'
 import { formSearch } from './them.js'
 import { pageNamber } from './them.js'
+import { quantityPerPage } from './them.js'
 
 const url = 'https://pixabay.com/api/'
 
 export function fetchCollection() {
-   return fetch(`${url}?&key=${API_KEY}&q=${formSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNamber}`)
+   return fetch(`${url}?&key=${API_KEY}&q=${formSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${quantityPerPage}&page=${pageNamber}`)
       .then(response => {
          if (!response.ok) {
              throw new Error(response.status);
          }
-         console.log(response);
+         // console.log(response);
          return response.json();
       })
 };
--- src/js/them.js
+++ src/js/them.js
@@ -1,31 +1,45 @@
 import { fetchCollection } from './fetchCollection.js'
 import { refs } from './refs.js'
-
 import collectionCardTpl from '../templates/collection.hbs'
 import Notiflix from "notiflix";
 
 export let formSearch = ''
 export let pageNamber = 1
+export let quantityPerPage = 40
+
+let totalPerPage = 0;
+const DEBOUNCE_DELAY = 300;
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
 
-// Поиск по заначению из инпута
+
+//////////////// Поиск по заначению из инпута
 export function onSearch(e) {
    e.preventDefault();
    pageNamber = 1
+   totalPerPage = 0
    formSearch = e.currentTarget.elements.searchQuery.value;
-      if (formSearch === "") {
+   if (formSearch === "") {
       clearsMarkup()
+      noSearchQuery(Notiflix)
+      // return
    } else {
       dataСall()
    }
-
 };
 
-// Кнопка загрузить еще...
+//////////////// Очистка списка
+function clearsMarkup() {
+   refs.btnLoadMore.classList.add('js_hidden')
+   refs.gallery.innerHTML = '';
+}
+  
+
+//////////////// Кнопка "загрузить еще" - нажатие...
 function onLoadMore() {
    pageNamber += 1;
+   refs.btnLoadMore.classList.add('js_hidden')
    dataСall();
 };
 
@@ -39,14 +53,72 @@ function dataСall() {
       })
 };
 
-//Разметка и рендез колелкции  
-function renderCollection(hits) {
-   const markup = collectionCardTpl(hits);
-   refs.gallery.innerHTML = markup;
+
+//////////////// Разметка и рендеp колелкции  
+function renderCollection(picture) {
+   const collectionObject = picture.hits;
+   const collectionLength = Object.keys(collectionObject).length;
+   const totalPicture = picture.totalHits
+   totalPerPage += collectionLength;
+
+   setTimeout(() => {
+      if (collectionLength === 0 && totalPicture === 0) {
+         clearsMarkup()
+         noSearchQuery(Notiflix)
+         return
+      }
+      if (collectionLength !== 0 && pageNamber === 1) {
+         // loadNatiflix(Notiflix)
+         Notiflix.Notify.success(`Hooray! We found ${totalPicture} images.`)
+      }
+      if (collectionLength > 0 && pageNamber === 1)  {
+         const markup = collectionCardTpl(picture);
+         refs.gallery.innerHTML = markup;
+         buttonVisible();
+      }
+      if (collectionLength > 0 && pageNamber > 1)  {
+         const markup = collectionCardTpl(picture);
+         refs.gallery.insertAdjacentHTML('beforeend', markup)
+         buttonVisible();
+      }
+      if (totalPerPage === totalPicture) {
+         refs.btnLoadMore.classList.add('js_hidden');
+         endSearchQuery(Notiflix)
+      }
+       
+   }, DEBOUNCE_DELAY);
 };
 
-// очистка списка
-function clearsMarkup() {
-   refs.gallery.innerHTML = "";
+
+//////////////// Нотифкации - Notiflix
+// Не найдено по запросу - Сообщение
+function noSearchQuery() {
+   Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
 }
+
+// Количестко найденых картинок
+// function AllSearchQuery() {
+//    Notiflix.Notify.success(`Hooray! We found ${totalPicture} images.`);
+// }
+
+// function endSearchQuery(Notiflix)
+// Конец галереи
+function endSearchQuery() {
+   Notiflix.Notify.info("We're sorry, but (you've) reached the end of search results.");
+}
+
+// загрузка 
+function loadNatiflix () {
+   Notiflix.Loading.circle('Please wait...');
+}
+
+
+//////////////// Кнопка "загрузить еще" -поведение
+refs.btnLoadMore.classList.add('js_hidden') // изначально скрывает видимость
+
+function buttonVisible () {
+   return refs.btnLoadMore.classList.remove('js_hidden') // возвращяет видимость кнопки
+}
+
+
 
